4862561280e2fc3398b867e3efbab1f3ce301eb7
Micha <micha.pdm@gmx.de>
2021-10-18 22:01:39 +0200

    feat: basic error handling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { Component, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { Box, Container } from "@mui/material";
+import { Container } from "@mui/material";
 import MyCard from "./Components/MaCard";
 import MapPosition from "./Components/MapPosition";
 // import 'mapbox-gl/dist/mapbox-gl.css';
@@ -8,31 +8,41 @@ import MapPosition from "./Components/MapPosition";
 export default function App() {
   const { REACT_APP_ACCESS_TOKEN: apiKey } = process.env;
   const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}`;
-  // let receivedData = {};
   const [receivedData, setReceivedData] = useState({});
+  const [statusLoading, setStatusLoading] = useState(true);
+
+  const errorHandler = (error) => {
+    console.log(`Error Message: ${error.message}`); 
+    alert(`Error Message: ${error.message}`);
+    // ToDo: does not actual handle the error
+    // ToDo: redirect to error-page
+  };
 
   useEffect(() => {
-    // fetch(url)
-    fetch("./mockdata.json")
+    fetch(url)
+    // fetch("./mockdata.json")
       .then(
         (res) => {
           console.log({ responseStatus: res.status });
           if (!res.ok)
             throw new Error(`An error has occured. HTTP code: ${res.status}`);
           return res.json();
-        },
-        (err) => console.log({ first: err.message })
-      )
-      .then((data) => setReceivedData(data))
-      .catch((error) => console.log({ second: error.message }));
-  }, [url]);
+        }, errorHandler)
+      .then(
+        (data) => {
+          setReceivedData(data);
+          setStatusLoading(false);
+        })
+      .catch(errorHandler);
+  
+  }, [url, statusLoading]);
 
-  return (
-    <div className="App">
-      <Container maxWidth="sm">
-        <MyCard {...receivedData} />
-        {receivedData.location && <MapPosition locationLat={receivedData.location.lat} locationLng={receivedData.location.lng} />}
-      </Container>
-    </div>
-  );
+return (
+  <div className="App">
+    <Container maxWidth="sm">
+      {receivedData && <MyCard {...receivedData} status={statusLoading} />}
+      {receivedData && <MapPosition locationLat={receivedData.location?.lat} locationLng={receivedData.location?.lng} />}
+    </Container>
+  </div>
+);
 }
